08e28f82915d996971a57f6e01f4cf304f0e20b9
Lyn Nagara <lyn.nagara@gmail.com>
2019-03-22 23:50:31 -0700

    fix get path

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,38 @@ use structopt::StructOpt;
 struct Opt {
     /// Depth (0 = max)
     #[structopt(short = "d", long = "depth", default_value = "1")]
-    depth: u8
+    depth: u8,
+
+    /// Root directory
+    directory: Option<String>,
 }
 
 fn main() {
-    // TODO: handle error
-    let path = get_path().unwrap();
-
     let opt = Opt::from_args();
 
-    if !&path.is_dir() {
-        println!("Invalid path");
-        std::process::exit(1);
-    }
+    let path = get_path(opt.directory);
 
-    let depth = if opt.depth == 0 {None} else {Some(opt.depth)};
+    match path {
+        Ok(p) => {
+            let depth = if opt.depth == 0 {None} else {Some(opt.depth)};
+            walk_folder(p, 1, depth);
+        },
+        Err(e) => handle_error(e),
+    };
+}
 
-    walk_folder(path, 0, depth)
+fn handle_error(err: Error) {
+    println!("{}", err.details);
+    std::process::exit(1);
 }
 
 fn walk_folder(folder_path: PathBuf, current_depth: usize, depth: Option<u8>) -> () {
-    if let Ok(entries) = fs::read_dir(folder_path) {
+    if let Ok(entries) = fs::read_dir(&folder_path) {
+
+        if current_depth == 1 {
+            println!("{}", &folder_path.display());
+        }
+
         for e in entries {
             let path = e.unwrap().path();
             if path.is_dir() {
@@ -40,34 +51,44 @@ fn walk_folder(folder_path: PathBuf, current_depth: usize, depth: Option<u8>) ->
                     return;
                 }
 
-                println!("{}{}", "  ".repeat(current_depth), &path.display());
+                println!("{}{}", "  ".repeat(current_depth), path.file_name().unwrap().to_string_lossy());
+
                 walk_folder(path, next_depth, remaining_depth);
 
             } else {
-                println!("{}{}", "  ".repeat(current_depth), &path.display());
+                println!("{}{}", "  ".repeat(current_depth), &path.file_name().unwrap().to_string_lossy());
             }
         }
     }
 }
 
+fn get_path(requested_path: Option<String>) -> Result<PathBuf, Error> {
+    match requested_path {
+        Some(p) => {
+            let path = PathBuf::from(p);
 
+            if path.is_dir() {
+                return Ok(path)
+            }
 
-#[derive(Debug)]
-pub enum Error {
-//   Os(),
+            let message = format!("{} is not a directory", path.display());
+
+            return Err(Error::new(&message))
+
+        },
+        None => {
+            return Ok(env::current_dir().unwrap())
+        },
+    };
 }
 
-fn get_path() -> Result<PathBuf, Error> {
-    if env::args_os().len() > 1 {
-        let arg = env::args_os().nth(1).unwrap().into_string().unwrap();
-        // TODO: properly detect if it's a path
-        if !arg.starts_with('-'){
-            let p =  PathBuf::from(&arg);
-            return Ok(p);
-        }
-    }
+#[derive(Debug)]
+struct Error {
+    details: String
+}
 
-    let current_path = env::current_dir().unwrap();
-    
-    Ok(current_path)
+impl Error {
+    fn new(msg: &str) -> Error {
+        Error{details: msg.to_string()}
+    }
 }
